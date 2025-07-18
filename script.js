58702ebff87dd03d7b3d424c50b8de8601bc14f4
andreeetmt <164437601+andreeetmt@users.noreply.github.com>
2025-07-18 00:20:04 -0300

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const contratoEndereco = "0xDA0bab807633f07f013f94DD0E6A4F96F8742B53; // contrato implantado
+const contratoEndereco = "0xDA0bab807633f07f013f94DD0E6A4F96F8742B53"; // contrato implantado
 
 const abiContrato = [
   {
